06b5b2471d4fefaa5384412424678c7e5d9942dd
Weining Li <bill.weining.li@gmail.com>
2018-08-18 09:16:20 -0400

    Update minheap.c

--- minheap/minheap.c
+++ minheap/minheap.c
@@ -4,7 +4,6 @@
 
 // minheap_create() creates a new empty minheap
 // effects: allocates memory (client must call minheap_destroy)
-
 struct minheap *minheap_create(void) {
   struct minheap *h = malloc(sizeof(struct minheap));
   h->len = 0;
@@ -15,7 +14,6 @@ struct minheap *minheap_create(void) {
 
 // minheap_is_empty(h) determines if heap h is empty
 // time: O(1)
-
 bool minheap_is_empty(const struct minheap *h) {
   assert(h);
   return h->len == 0;
@@ -24,28 +22,29 @@ bool minheap_is_empty(const struct minheap *h) {
 // minheap_insert(i, h) adds i to heap h
 // effects: h is modified
 // time:    O(logn), n is the size of the heap
-
 void minheap_insert(int i, struct minheap *h) {
   assert(h);
   if(h->len == h->maxlen) {
     h->maxlen = h->maxlen * 2 + 1;
     h->data = realloc(h->data, h->maxlen * sizeof(int));
   }
+  
   h->data[h->len] = i;
   int pos = h->len;
+  
   while(h->data[pos] < h->data[(pos - 1)/2]) {
     h->data[pos] = h->data[(pos - 1)/2];
     h->data[(pos - 1)/2] = i;
     pos = (pos - 1)/2;
     if (pos == 0) break;
   }
+  
   h->len += 1;
 }
 
 // minheap_top(h) returns the top of the heap
 // requires: h is not empty
 // time:     O(1)
-
 int minheap_top(const struct minheap *h) {
   assert(h);
   assert(h->len);
@@ -56,7 +55,6 @@ int minheap_top(const struct minheap *h) {
 // requires: h is not empty
 // effects:  h is modified
 // time:     O(logn), n is the size of the heap
-
 int minheap_remove(struct minheap *h) {
   assert(h);
   assert(h->len);
@@ -64,14 +62,18 @@ int minheap_remove(struct minheap *h) {
   h->data[0] = h->data[h->len - 1];
   h->len -= 1;
   int pos = 0;
+  
   while ((pos * 2 + 1 < h->len && h->data[pos * 2 + 1] < h->data[pos]) ||
          (pos * 2 + 2 < h->len && h->data[pos * 2 + 2] < h->data[pos])) {
+    
     if (h->data[pos * 2 + 1] <= h->data[pos * 2 + 2]) {
       int temp = h->data[pos * 2 + 1];
       h->data[pos * 2 + 1] = h->data[pos];
       h->data[pos] = temp;
       pos = pos * 2 + 1;
-    } else {
+    } 
+    
+    else {
       int temp = h->data[pos * 2 + 2];
       h->data[pos * 2 + 2] = h->data[pos];
       h->data[pos] = temp;
@@ -84,7 +86,6 @@ int minheap_remove(struct minheap *h) {
 // minheap_destroy(h) frees all memory for h
 // effects: h is no longer valid
 // time:    O(1)
-
 void minheap_destroy(struct minheap *h) {
   assert(h);
   free(h->data);
@@ -95,7 +96,6 @@ void minheap_destroy(struct minheap *h) {
 // requires: a is a valid array, len > 0
 // effects:  modifies a
 // time:     O(nlogn)
-
 void heapsort(int a[], int len) {
   assert(a);
   assert(len > 0);
@@ -103,8 +103,10 @@ void heapsort(int a[], int len) {
   for (int i = 0; i < len; ++i) {
     minheap_insert(a[i], h);
   }
+  
   for (int j = 0; j < len; ++j) {
     a[j] = minheap_remove(h);
   }
+  
   minheap_destroy(h);
 }
