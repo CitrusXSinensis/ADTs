197564d8277f88404ef32a58e796c1d6948d094b
WeiningLi <bill.weining.li@gmail.com>
2018-12-25 21:42:26 -0500

    Create spell_check.c

---
+++ word_suggester/spell_check.c
@@ -0,0 +1,11 @@
+#include "spell_check.h"
+#include <stdbool>
+
+// Clients are responsible to build their own dictionary for different usages
+
+// spellcheck(word) determines if word is spelled correctly
+// requires: word is a valid string of only lowercase letters
+// time:     O(n), where n is the length of word
+bool spellcheck(const char *word) {
+  return true;
+}
