c1e522c8d96566d6f60b67b5d86d468c3149e3f5
WeiningLi <bill.weining.li@gmail.com>
2019-01-03 23:14:01 -0500

    [sorting] add selection sort method

--- sorting.c
+++ sorting.c
@@ -1,1 +1,21 @@
 // Elementary sorting methods
+
+swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// worst case: O(n^2)
+void selection_sort(int a[], int len) {
+  int pos = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    pos = i;
+    for (int j = i + 1; j < len; ++j) {
+      if (a[j] < a[pos]) {
+        pos = j;
+      }
+    }
+    swap(&a[i], &a[pos]);
+  }
+}
